106cb4989d4830540922a5a405b6e048a2125ab5
Dipty Singhania <dsinghania@yahoo.com>
2016-02-16 13:41:20 -0600

    Assignment_Unit3

--- Assignment_Unit3/index.js
+++ Assignment_Unit3/index.js
@@ -1,44 +1,48 @@
+//To use the modules we require process, filesystem, path and colors 
 var process = require("process");
-var thirdParameter = process.argv[2];
-//console.log(thirdParameter);
-
 var fs = require('fs');
 var path = require('path');
 var colors = require("colors");
 
-var file = fs.readdirSync("./logs/raw");
-//console.log(file);
-//console.log(file[0]);
-for(var i = 0; i < file.length; i++) {
+//Obtain the location of the logs folder from the 3rd parameter
+// passed into the command line statement
+var thirdParameter = process.argv[2];
+//console.log(thirdParameter);  
+
+//Creates a folder named “processed” inside the “logs” folder
+fs.mkdirSync(thirdParameter + "/processed");
+
+//Creates a folder for each year prefix
+//on the log files so that the logs can be sorted by year
+fs.mkdirSync(thirdParameter + "/processed/2014");
+fs.mkdirSync(thirdParameter + "/processed/2015");
+fs.mkdirSync(thirdParameter + "/processed/2016");
+
+//reads the content of the directory, returns an array of filenames
+var fileNew = fs.readdirSync(thirdParameter + "/raw");
+
+//Sort the log files into the folders that match the year in their filename
+for(var i = 0; i < fileNew.length; i++) {
     
-    if ( file[i].startsWith("2014") ) {
-       // console.log("inside if of 2014");
-        fs.renameSync("logs/raw/" + file[i], "logs/processed/2014/" + file[i] );
-        // path.join("logs","processed", "2014", file[i]) );
+    if ( fileNew[i].startsWith("2014") ) {
+        fs.renameSync("logs/raw/" + fileNew[i], "logs/processed/2014/" + fileNew[i] );
     }
-    else if( file[i].startsWith("2015") ) {
-        fs.renameSync("logs/raw/" + file[i],  "logs/processed/2015/" + file[i] );
-        //path.join("logs","processed", "2015", file[i]) );
+    else if( fileNew[i].startsWith("2015") ) {
+        fs.renameSync("logs/raw/" + fileNew[i],  "logs/processed/2015/" + fileNew[i] );
     }
-    else if( file[i].startsWith("2016") ) {
-        fs.renameSync("logs/raw/" + file[i],  "logs/processed/2016/" + file[i] );
-        //path.join("logs","processed", "2016", file[i]) );
+    else if( fileNew[i].startsWith("2016") ) {
+        fs.renameSync("logs/raw/" + fileNew[i],  "logs/processed/2016/" + fileNew[i] );
     }
 }
+
+//count the total number of files that were sorted into each folder
 var count2014 = fs.readdirSync("./logs/processed/2014");
 var count2015 = fs.readdirSync("./logs/processed/2015");
 var count2016 = fs.readdirSync("./logs/processed/2016");
+
+//Color the log messages and write to the console the number of files in each folder
 console.log(colors.green("sorting files..."));
 console.log(colors.blue("moved [" + count2014.length + "] logs into processed\\2014"));
 console.log(colors.blue("moved [" + count2015.length + "] logs into processed\\2015"));
 console.log(colors.blue("moved [" + count2016.length + "] logs into processed\\2016"));
 console.log(colors.green("...finished!"));
-
-
-//var fse = require('fs-extra');
-//var count1 = 0, count2 = 0, count3 = 0;
-//var mv = require('mv');
-//mv(./logs/raw/2014*, ./logs/raw/2014/,function(err) {});
-//fs.renameSync("./logs/raw/2014* ","./logs/raw/2014/");
-//var fileName = ("./logs/raw/*").split(path.delimiter)[0];
-//console.log(fileName);
