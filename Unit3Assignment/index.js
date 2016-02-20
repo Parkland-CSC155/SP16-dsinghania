02b1642d953707b8828c8b6f929b27abf9ffd34b
Dipty Singhania <dsinghania@yahoo.com>
2016-02-20 09:11:09 -0600

    Unit3Assignment

--- Unit3Assignment/index.js
+++ Unit3Assignment/index.js
@@ -1,5 +1,5 @@
 //We need to use require to use the modules- process, filesystem, path and colors 
-
+console.log("starting...");
 var DEBUG = false; //make it true while debugging
 var process = require("process");
 var fs = require('fs');
@@ -10,17 +10,14 @@ var colors = require("colors");
 // passed into the command line statement
 var logsFolder = process.argv[2];
 
-//console.log(logsFolder);  //if DEBUG is true make it log(logsFolder to debug the errors)
+//console.log(logsFolder);  //if DEBUG is true make it log(logsFolder) to debug the errors
 
-//Creates a folder named “processed” inside the “logs” folder
-
-    //var processedFolder = path.join(logFolder, "/processed");
-    //fs.mkdirSync(processedFolder)
-    //var processedFolder2 = logsFolder + "/processed";
+console.log("creating[processed] directories if not exists...");
 
 //ensures that relative paths are resolved to absolute paths
 var absPath = path.resolve(logsFolder);
 
+//Creates a folder named “processed” inside the “logs” folder
 var processedFolder = path.join(logsFolder, "processed");
 fs.mkdirSync(processedFolder);
 
@@ -42,6 +39,8 @@ fs.mkdirSync(folder2016);
 
 //reads the content of the directory, returns an array of filenames in the raw directory
 var rawFilesArray = fs.readdirSync(logsFolder + "/raw");
+
+console.log(colors.green("sorting files..."));
 
 //Sort the log files into the folders that match the year in their filename
 for(var i = 0; i < rawFilesArray.length; i++) {
@@ -72,7 +71,7 @@ var fileArray2015 = fs.readdirSync("./logs/processed/2015");
 var fileArray2016 = fs.readdirSync("./logs/processed/2016");
 
 //Color the log messages and write to the console the number of files in each folder
-console.log(colors.green("sorting files..."));
+
 console.log(colors.blue("moved [" + fileArray2014.length + "] logs into processed\\2014"));
 console.log(colors.blue("moved [" + fileArray2015.length + "] logs into processed\\2015"));
 console.log(colors.blue("moved [" + fileArray2016.length + "] logs into processed\\2016"));
