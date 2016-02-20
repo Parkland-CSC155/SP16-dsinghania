3962ba2dc21154a4792e00cf901a01479da85104
Dipty Singhania <dsinghania@yahoo.com>
2016-02-20 08:59:59 -0600

    Unit4Exercise

--- Unit4Exercise/index.js
+++ Unit4Exercise/index.js
@@ -7,39 +7,39 @@ if (process.argv.indexOf("-v") > -1) {
     //the -v flag has been included
 }
 //get the input folder name
+var inputIndex, inputFile ;
 if(process.argv.indexOf("-folder") >= 0) {
-    
-    var inputIndex = process.argv.indexOf("-folder");
+     //the -input flag has been included    
+    inputIndex = process.argv.indexOf("-folder");
     inputIndex += 1;
-    var inputFile = process.argv[inputIndex];
+    inputFile = process.argv[inputIndex];
 }
 //handle the file output flag
+var argIndex, outputFile;
 if (process.argv.indexOf("-output") > -1) {
     
-    //the -v\output flag has been included
-    var argIndex = process.argv.indexOf("-output");
+    //the -output flag has been included
+    argIndex = process.argv.indexOf("-output");
     argIndex = argIndex + 1;
     
     //this grabs the next parameter inn out arguments array
     //e.g. -output "c:\temp\logs\file.txt"
-    var outputFile = process.argv[argIndex];
+    outputFile = process.argv[argIndex];
    // console.log(outputFile);
 }
 
 var inputFolder = path.resolve(inputFile);
-//var inputFolder = path.join(__dirname, "input") ;
-//var inputFolder = path.join(__dirname, inputFile) ;
 console.log(inputFolder);
 
-var fileArray = fs.readdirSync(inputFolder);
+var inputFileArray = fs.readdirSync(inputFolder);
 //console.log(fileArray);
 
 var newPath = path.join(__dirname, outputFile);
 //console.log(newPath);
 
-for(var i = 0; i < 2; i++ ){ //fileArray.length
+for(var i = 0; i < inputFileArray.length; i++ ){ //
 
-    var fullPath = path.join(inputFolder, fileArray[i]);
+    var fullPath = path.join(inputFolder, inputFileArray[i]);
     
     var contents = fs.readFileSync(fullPath);
     //console.log(contents);
