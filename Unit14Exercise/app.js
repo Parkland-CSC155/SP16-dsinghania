1f7eec70d88291b51dbdf3318490f3caf417179a
Dipty Singhania <dsinghania@yahoo.com>
2016-04-13 22:30:19 -0500

    Unit 14 Exercise

--- Unit14Exercise/app.js
+++ Unit14Exercise/app.js
@@ -34,23 +34,26 @@ app.get("/episodes", function(req, res){
 
 app.get("/episodes/:id", function(req, res){
     var id = req.params.id;
-    //var data = getData(id);
+    id = Number(id);            //convert string to number
+    res.setHeader("Content-Type", "text/html");
     var folder = path.join(__dirname , "data/breaking_bad.json");
     fs.readFile(folder, "utf-8",function(err, contents){
         if(err){
             console.log(err);
-            return;
+            next(err);
         } 
         var data = JSON.parse(contents);
-        for (var i = 0 ; i <  data._embedded.episodes.length; i++){            
-            if(id === data._embedded.episodes[i].id ){
-                var newData = data._embedded.episodes[i];
-                console.log(newData);
-                res.render("details-example", {epiName: newData.name,
-                    epiSeason: newData.season, epiNumber: newData.number,
-                    epiAirDate:newData.airdate, epiSummary: newData.summary}); 
-                return;
-            }
+        var newData = getEpisodesById(data, id);
+        var sum = newData.summary;
+        if(newData){
+            res.render("details-example", {epiName: newData.name,
+                epiSeason: newData.season, epiNumber: newData.number,
+                epiAirDate:newData.airdate, epiSummary: newData.summary}); 
+            return;
+        } else {
+            var err = new Error("Page Not Found!");
+            err.status = 404;
+            next(err);  
         }
     });
 });
@@ -72,31 +75,11 @@ app.listen(process.env.PORT || 3000, function(){
     console.log("Exercise app listening on port 3000!");   
 });
 
-function getData(id){
-    var folder = path.join(__dirname , "data/breaking_bad.json");
-    var data;
-    fs.readFile(folder, "utf-8",function(err, contents){
-        if(err){
-            console.log(err);
-            return;
-        } 
-        data = JSON.parse(contents);
-        //res.setHeader("Content-Type", "application/json charset=UTF-8"); 
-        
-        for (var i = 0 ; i <  data._embedded.episodes.length; i++){
-            
+function getEpisodesById(data, id){
+    for (var i = 0 ; i <  data._embedded.episodes.length; i++){            
             if(id === data._embedded.episodes[i].id ){
-                return  data._embedded.episodes[i];
+                var newData = data._embedded.episodes[i];
+                return newData;
             }
-           // var name = data._embedded.episodes[i].name;
-            //var season =  data._embedded.episodes[i].season;
-           // var num =  data._embedded.episodes[i].number;
-           // var airDate =  data._embedded.episodes[i].airdate;
-            //var summary = data._embedded.episodes[i].summary;
-           // res.render("index-example", {epiName: name, epiSeason: season, epiNumber: "Episode " + num, epiAirDate: airDate, epiSummary: summary}); 
-        }
-        
-        //res.end(contents);       
-    });
-   // return data;
+    }
 }
