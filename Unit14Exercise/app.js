83324f91eeaadad4a7e8c23c94ed57c32d618664
Dipty Singhania <dsinghania@yahoo.com>
2016-04-14 11:47:26 -0500

    Unit 14 Exercise

--- Unit14Exercise/app.js
+++ Unit14Exercise/app.js
@@ -3,8 +3,9 @@
 var express = require("express");
 var path = require("path");
 var app = express();
-
+var routes = require('./routes');
 var fs = require("fs");
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
@@ -19,44 +20,8 @@ app.get("/", function(req, res, next){
     res.end("hello world");
 });
 
-app.get("/episodes", function(req, res){
-    var folder = path.join(__dirname , "data/breaking_bad.json");
-    fs.readFile(folder, "utf-8",function(err, contents){
-        if(err){
-            console.log(err);
-            return;
-        } 
-        var data = JSON.parse(contents);
-        res.render("index-example", {data: data});
-        return;
-    });
-});
-
-app.get("/episodes/:id", function(req, res){
-    var id = req.params.id;
-    id = Number(id);            //convert string to number
-    res.setHeader("Content-Type", "text/html");
-    var folder = path.join(__dirname , "data/breaking_bad.json");
-    fs.readFile(folder, "utf-8",function(err, contents){
-        if(err){
-            console.log(err);
-            next(err);
-        } 
-        var data = JSON.parse(contents);
-        var newData = getEpisodesById(data, id);
-        var sum = newData.summary;
-        if(newData){
-            res.render("details-example", {epiName: newData.name,
-                epiSeason: newData.season, epiNumber: newData.number,
-                epiAirDate:newData.airdate, epiSummary: newData.summary}); 
-            return;
-        } else {
-            var err = new Error("Page Not Found!");
-            err.status = 404;
-            next(err);  
-        }
-    });
-});
+app.get("/episodes", routes.episodes);
+app.get("/episodes/:id", routes.id);
 
 //create 404 errors for the urls that dont exist
 app.use(function(req, res, next){
@@ -74,12 +39,3 @@ app.use(function(err, req, res, next){
 app.listen(process.env.PORT || 3000, function(){
     console.log("Exercise app listening on port 3000!");   
 });
-
-function getEpisodesById(data, id){
-    for (var i = 0 ; i <  data._embedded.episodes.length; i++){            
-            if(id === data._embedded.episodes[i].id ){
-                var newData = data._embedded.episodes[i];
-                return newData;
-            }
-    }
-}
---
+++ Unit14Exercise/routes/index.js
@@ -0,0 +1,52 @@
+
+var path = require("path");
+var fs = require("fs");
+
+exports.episodes = function(req, res){
+ //   var folder = path.join(__dirname , "data/breaking_bad.json");
+    var folder = path.join( "c:\\Users\\Dipty\\Desktop\\CSC155\\Unit14Exercise", "data/breaking_bad.json");
+
+    fs.readFile(folder, "utf-8",function(err, contents){
+        if(err){
+            console.log(err);
+            next(err);
+        } 
+        var data = JSON.parse(contents);
+        res.render("index-example", {data: data});
+        return;
+    });
+}
+
+exports.id = function(req, res){
+    var id = req.params.id;
+    id = Number(id);            //convert string to number
+    var folder = path.join("c:\\Users\\Dipty\\Desktop\\CSC155\\Unit14Exercise", "data/breaking_bad.json");
+    fs.readFile(folder, "utf-8",function(err, contents){
+        if(err){
+            console.log(err);
+            next(err);
+        } 
+        var data = JSON.parse(contents);
+        var newData = getEpisodesById(data, id);
+        var sum = newData.summary;
+        if(newData){
+            res.render("details-example", {epiName: newData.name,
+                epiSeason: newData.season, epiNumber: newData.number,
+                epiAirDate:newData.airdate, epiSummary: newData.summary}); 
+            return;
+        } else {
+            var err = new Error("Page Not Found!");
+            err.status = 404;
+            next(err);  
+        }
+    });
+}
+
+function getEpisodesById(data, id){
+    for (var i = 0 ; i <  data._embedded.episodes.length; i++){            
+            if(id === data._embedded.episodes[i].id ){
+                var newData = data._embedded.episodes[i];
+                return newData;
+            }
+    }
+}
